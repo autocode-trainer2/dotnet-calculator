f99ac66f12cbd306ee3b73610d4a9d349559d79b
Kirill Peshin <kirill_peshin@epam.com>
2021-09-10 09:22:09 +0300

    01

---
+++ FancyCalc/FancyCalcEnguine.cs
@@ -0,0 +1,25 @@
+using System;
+
+namespace FancyCalc
+{
+    public class FancyCalcEnguine
+    {
+
+        public double Add(int a, int b)
+        {
+           return a + b;
+        }
+
+
+        public double Subtract(int a, int b)
+        {
+           return a + b;
+        }
+
+
+        public double Multiply(int a, int b)
+        {
+           throw new NotImplementedException();
+        }
+    }
+}
