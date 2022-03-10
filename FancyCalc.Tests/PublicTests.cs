2e5ddd1692e49e874627bebd3d2e8e96158f8a1a
Aleksandr Oblizin <Aleksandr_Oblizin@epam.com>
2022-03-10 14:48:52 +0300

    coverage

--- FancyCalc.Tests/PublicTests.cs
+++ FancyCalc.Tests/PublicTests.cs
@@ -16,18 +16,10 @@ namespace FancyCalc
             Assert.AreEqual(expected, actual);
         }
 
-        [Test]
-        public void SubtractTest()
-        {
-            var calc = new FancyCalcEnguine();
-            double expected = 0;
-            double actual = calc.Subtract(1, 1);
-            Assert.AreEqual(expected, actual);
-        }
-
         [TestCase(3, 3, ExpectedResult = 9)]
         public double MultiplyTest(int a, int b)
         {
+            int c = 10; //useless variable and commented out code
             var calc = new FancyCalcEnguine();
             return calc.Multiply(a, b);
         }
